0f28483ae4de29e75c46d3ba103db1f7fc5af54e
AdrieanKhisbe <adriean.khisbe@live.fr>
2015-12-19 06:59:34 +0100

    Fix package metadata, cf purcell recommandation

--- fireplace.el
+++ fireplace.el
@@ -1,9 +1,29 @@
-;;; fireplace.el --- A cozy fireplace for emacs
-;;; Version: 1.0
+;;; fireplace.el --- A cozy fireplace for emacs      -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2015 Johan Sivertsen
+;;; Version: 0.1
+;;; Author: Johan Sivertsen <johanvts@gmail.com>
+;;; URL: https://github.com/johanvts/emacs-fireplace
+;;; Released: December 2015
+
+;; Keywords: games
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;;; Commentary:
-;; Author: Johan Sivertsen <johanvts@gmail.com>
-;; URL: https://github.com/johanvts/emacs-fireplace
-;; Released: December 2015
+
+;; Puts your emacs on fire
 
 ;;; Code:
 ;; User definable Variables
