72c44c0b59d52f054c7500d7112b56216b27a8b6
Nick Drozd <nicholasdrozd@gmail.com>
2018-12-08 13:08:01 -0600

    Add failing test-fire-raging-out-of-control

--- test/emacs-fireplace-test.el
+++ test/emacs-fireplace-test.el
@@ -3,8 +3,20 @@
 ;;; Commentary:
 ;; These are the tests for `emacs-fireplace'
 
+;; Run with shell command:
+
+;; emacs -Q --batch --directory ./ --directory ./test --load test/emacs-fireplace-test.el --funcall ert-run-tests-batch-and-exit
+
 ;;; Code:
 
-(ert-deftest emacs-fireplace-should-not-pass ()
-  (should-not nil))
+(require 'fireplace)
 
+(ert-deftest test-fire-raging-out-of-control ()
+  :expected-result :failed
+  (fireplace t)
+  (let ((buf (get-buffer "*fireplace*")))
+    (should buf)
+    (kill-buffer buf))
+  (sit-for 2)
+  (let ((buf (get-buffer "*fireplace*")))
+    (should-not buf)))
